3cbc7c2cef7c2c74b4423d30b9536733be7238e2
aramirezram <aramirezram@unal.edu.co>
2023-02-24 08:43:31 -0500

    Intento5:Java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -48,10 +48,10 @@ public class TV {
 		this.canal=canal;
 	}
 	
-	public int getNumTV() {
+	public static int getNumTV() {
 		return numTV;
 	}
-	public void setNumTV(int numTV) {
+	public static void setNumTV(int numTV) {
 		TV.numTV=numTV;
 	}
 	
